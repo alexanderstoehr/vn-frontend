a9062f1c965ce9ac4dc54ef88c625b74f6740e35
Alexander Stöhr <git@alexander-stoehr.de>
2024-10-27 12:48:17 +0100

    rearranged pages folder and components

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,19 +1,19 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout.jsx";
 import PageNotFound from "./PageNotFound.jsx";
-import Home from "./Home.jsx";
+import Home from "./Home/Home.jsx";
 import ProtectedRoute from "../layouts/ProtectedRoute.jsx";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<DefaultLayout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<PageNotFound />} />
-                        <Route element={<ProtectedRoute />}>
-                            <Route path="/account" element={<h1>Protected</h1>}/>
-                        </ Route >
+                <Route element={<DefaultLayout/>}>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="*" element={<PageNotFound/>}/>
+                    <Route element={<ProtectedRoute/>}>
+                        <Route path="/account" element={<h1>Protected</h1>}/>
+                    </ Route>
                 </Route>
             </Routes>
         </BrowserRouter>
