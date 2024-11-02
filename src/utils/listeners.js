c97c28094a12c2c96d2ced83b90b1c33c7a231a6
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-02 15:48:58 +0100

    added eventlistener for key events

---
+++ src/utils/listeners.js
@@ -0,0 +1,36 @@
+import {shortkeyFunctions} from "./shortkeys.js";
+
+export const listenToKey = () => {
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'Space') {
+            shortkeyFunctions.playerPlayPause();
+        }
+        if (e.code === 'ArrowRight') {
+            shortkeyFunctions.playerTenForward(10);
+        }
+        if (e.code === 'ArrowLeft') {
+            shortkeyFunctions.playerTenBack(10);
+        }
+        if (e.code === 'KeyR') {
+            shortkeyFunctions.playerReplayCurrentNote();
+        }
+        if (e.code === 'KeyN') {
+            shortkeyFunctions.playerJumpToNextNote();
+        }
+        if (e.code === 'KeyP') {
+            shortkeyFunctions.playerJumpToPreviousNote();
+        }
+        if (e.code === 'KeyM') {
+            shortkeyFunctions.playerMute();
+        }
+        if (e.code === 'KeyS') {
+            shortkeyFunctions.playerSwitchSubtitles();
+        }
+        if (e.code === 'KeyA') {
+            shortkeyFunctions.noteAddNote();
+        }
+        if (e.code === 'KeyD') {
+            shortkeyFunctions.noteFocusDescription();
+        }
+    });
+}
