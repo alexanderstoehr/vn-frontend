8a2e9717fec3300d32f9345603701d39d13a3fdb
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-04 20:51:09 +0100

    added base benefits section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "./Hero.jsx";
 import Demo from "./Demo.jsx";
+import Benefits from "./Benefits.jsx";
 
 export default function Home() {
     return (
         <div>
             <Hero/>
             <Demo/>
+            <Benefits/>
         </div>
     )
 }
