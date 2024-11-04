6b4372b1fb44a152055a653d1ea1c3f814125d10
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-04 20:15:15 +0100

    added base header

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router-dom";
+import Header from "../components/Header.jsx";
 
 export default function DefaultLayout() {
     return (
         <>
-            <Outlet />
+            <Header/>
+            <Outlet/>
         </>
-    )
+    );
 }
