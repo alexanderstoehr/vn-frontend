a027cf269949829cfedf4a4ec651e31ed893735e
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-10 13:28:47 +0100

    updated protected routes and account header

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function DefaultLayout() {
-    return (
-        <>
-            <Header/>
-            <Outlet/>
-        </>
-    );
+	return (
+		<>
+			<Header/>
+			<Outlet/>
+			<Footer/>
+		</>
+	);
 }
