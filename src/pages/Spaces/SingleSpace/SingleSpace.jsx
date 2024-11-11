b5e8833d1be1bfe194f7f62fbfe860cac7c6efa6
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-11 15:13:29 +0100

    finished single space page with dummydata

--- src/pages/Spaces/SingleSpace/SingleSpace.jsx
+++ src/pages/Spaces/SingleSpace/SingleSpace.jsx
@@ -3,6 +3,7 @@ import {HiOutlineFolder, HiOutlineTag, HiOutlineBookmark, HiOutlineCalendar} fro
 import singleSpace from "../../../data/dummyData/singleSpace.json";
 import spaceTax from "../../../data/dummyData/allSpaceTax.json";
 import {useState} from "react";
+import Tag from "../../../components/primitives/Tag.jsx";
 
 export default function SingleSpace() {
 
@@ -81,17 +82,19 @@ export default function SingleSpace() {
 				</div>
 				{singleSpace.videos.map((video, index) => (
 					<div key={index}
-						 className="flex gap-8 border border-gray-300 rounded-md p-8 cursor-pointer hover:scale-[1.01] hover:ease-in-out transition-transform duration-300 bg-gray-200">
+						 className="flex gap-8 border border-gray-300 rounded-md p-8 cursor-pointer hover:scale-[1.01] hover:ease-in-out transition-transform duration-300 bg-gray-100">
 						<img src={video.thumbnail} className="w-96 h-56 object-cover rounded-xl"/>
 						<div>
 							<div>
-
-								{video.tags.map((tag, index) => (
-									<span key={index} className="text-sm text-primary-800">{tag.name}</span>
-								))}
+								<div className="mb-2">
+									{video.tags.map((tag, index) => (
+										<Tag key={index} text={tag.name} close=""/>
+									))}
+								</div>
 								<div className="flex flex-row gap-4">
-									<div className="flex flex-row items-center text-sm text-gray-700 dark:text-gray-400 gap-1">
-										<HiOutlineCalendar className="text-primary-800 w-4 h-4"/>
+									<div
+										className="flex flex-row items-center text-xs font-semibold text-gray-700 dark:text-gray-400 gap-1">
+										<HiOutlineCalendar className=" text-primary-800 w-4 h-4"/>
 										{video.created}
 									</div>
 									<div className="flex flex-row items-center text-sm text-gray-700 dark:text-gray-400 gap-1">
@@ -100,8 +103,8 @@ export default function SingleSpace() {
 									</div>
 								</div>
 							</div>
-							<div className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{video.title}</div>
-							<p className="font-normal text-gray-700 dark:text-gray-400">{video.description}</p>
+							<div className="text-lg font-semibold leading-tight text-gray-900 dark:text-white mb-2">{video.title}</div>
+							<p className="font-normal text-gray-500 dark:text-gray-400">{video.description}</p>
 						</div>
 					</div>
 				))}
