46b602c499ff233d2ecaa331c14cf803c8c505fd
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-14 21:15:05 +0100

    updated single video in space listing styling

--- src/pages/Spaces/AllSpaces/SpaceListingSingleVideo.jsx
+++ src/pages/Spaces/AllSpaces/SpaceListingSingleVideo.jsx
@@ -1,8 +1,10 @@
 import {HiOutlineBookmark, HiOutlineCalendar} from "react-icons/hi";
+import allSpaceTax from "../../../data/dummyData/allSpaceTax.json";
+import Tag from "../../../components/primitives/Tag.jsx";
 
 export default function SpaceListingSingleVideo({video}) {
 	return (
-		<div className="min-w-80">
+		<div className="min-w-80 rounded-xl border-2 p-6 hover:scale-[1.01] z-10 ease-in-out duration-200 cursor-pointer">
 			<img className="mb-4" src={video.thumbnail}></img>
 			<div className="flex flex-row gap-4 mb-2">
 				<div
@@ -15,8 +17,15 @@ export default function SpaceListingSingleVideo({video}) {
 					{video.notes.length} Notes
 				</div>
 			</div>
-			<div className="text-lg font-semibold leading-tight">
+			<div className="text-lg font-semibold leading-tight mb-2">
 				{video.title}
+			</div>
+			<div className="flex flex-wrap">
+				{video.tags.map((tag, index) => (
+					<div key={index}>
+						<Tag text={tag.name} close=""/>
+					</div>
+				))}
 			</div>
 		</div>
 	)
