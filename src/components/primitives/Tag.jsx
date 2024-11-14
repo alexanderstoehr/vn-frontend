cde9c6fe17b80278187c2969644662cbb305014b
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-14 21:14:09 +0100

    updated tag styling

--- src/components/primitives/Tag.jsx
+++ src/components/primitives/Tag.jsx
@@ -3,7 +3,7 @@ import {HiX} from "react-icons/hi";
 // eslint-disable-next-line react/prop-types
 export default function Tag({close, text}) {
 	return (
-		<div className="inline-flex flex-row items-center  bg-indigo-200 px-2 text-primary-800 rounded gap-2 mr-2">
+		<div className="inline-flex flex-row items-center  bg-indigo-100 px-2 text-indigo-800 rounded gap-2 mr-2 mb-2 cursor-pointer">
 			<span>{text}</span>
 			{close && <HiX className=" cursor-pointer -mb-[1px]"/>}
 		</div>
