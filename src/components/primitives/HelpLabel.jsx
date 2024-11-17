3054bd1dee1f4359c8677cdcbc509e2a5462654a
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-17 19:42:09 +0100

    fixed help label component

--- src/components/primitives/HelpLabel.jsx
+++ src/components/primitives/HelpLabel.jsx
@@ -1,8 +1,6 @@
-import {HiOutlineQuestionMarkCircle} from "react-icons/hi";
-
 export default function HelpLabel({text}) {
 	return (
-		<div className="flex flex-row items-center gap-2 text-xs text-gray-500 dark:text-gray-400">
+		<div className="flex flex-row items-center gap-2 text-xs text-gray-500 dark:text-gray-400 ml-4">
 			{/* <HiOutlineQuestionMarkCircle className="w-4 h-4"/> */}
 			{text}
 		</div>
