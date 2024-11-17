26469580c107cd772c9852a2ad1f92415d0284f9
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-17 23:44:18 +0100

    added basic wysiwig editor

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,3 +1,5 @@
+import React, {useEffect, useRef} from 'react';
+import {initializeEditor} from '../../utils/editor';
 import video from '../../data/dummyData/demoVideo.json';
 import {
 	HiArrowRight,
@@ -16,10 +18,18 @@ import videoCategories from "../../data/dummyData/videoCategories.json";
 import Tag from "../../components/primitives/Tag.jsx";
 import Note from "./Note.jsx";
 
-
 export default function Video() {
+	const noteDescriptionRef = useRef(null);
 
-	console.log(video)
+	useEffect(() => {
+		if (noteDescriptionRef.current) {
+			const editor = initializeEditor(noteDescriptionRef.current);
+
+			return () => {
+				editor.destroy();
+			};
+		}
+	}, []);
 
 	return (
 		<div>
@@ -28,8 +38,7 @@ export default function Video() {
 					<div className="">
 						<div className="">{video.title}</div>
 						<div className="flex flex-row gap-4">
-							<div
-								className="flex flex-row items-center text-xs font-semibold text-gray-500 dark:text-gray-400 gap-1">
+							<div className="flex flex-row items-center text-xs font-semibold text-gray-500 dark:text-gray-400 gap-1">
 								<HiOutlineCalendar className=" text-gray-500 w-4 h-4"/>
 								{video.created}
 							</div>
@@ -75,8 +84,11 @@ export default function Video() {
 									<div className="text-lg font-semibold mb-4">Creating the Meta Table (Placeholder)</div>
 								</div>
 								<div className="">
-									<textarea id="noteDesciption" className="w-full min-h-96"
-											  value="https://www.npmjs.com/package/suneditor"></textarea>
+                  <textarea
+					  id="noteDescription"
+					  ref={noteDescriptionRef}
+					  className="w-full min-h-96">
+                  </textarea>
 								</div>
 							</div>
 						</div>
@@ -96,9 +108,8 @@ export default function Video() {
 								<div className="text-lg font-semibold mr-4">Your Notes</div>
 								<div className="border rounded-xl border-gray-300">
 									{video.notes.map(note => (
-											<Note note={note.note} key={note.id} active={note.active} time={note.time}/>
-										)
-									)}
+										<Note note={note.note} key={note.id} active={note.active} time={note.time}/>
+									))}
 								</div>
 							</div>
 						</div>
@@ -113,9 +124,10 @@ export default function Video() {
 										value=""
 										placeholder="Add Tag"
 									/>
-									<div className="flex flex-wrap mt-4"> {video.tags.map(tag => (
-										<Tag text={tag.name} key={tag.id} close="true"/>
-									))}
+									<div className="flex flex-wrap mt-4">
+										{video.tags.map(tag => (
+											<Tag text={tag.name} key={tag.id} close="true"/>
+										))}
 									</div>
 								</div>
 								<div className="flex flex-col">
@@ -132,8 +144,7 @@ export default function Video() {
 						</div>
 					</div>
 				</div>
-
 			</div>
 		</div>
-	)
+	);
 }
