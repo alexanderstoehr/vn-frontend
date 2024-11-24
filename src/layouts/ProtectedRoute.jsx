74c00a4b4399f15402347ec14bd0b5d46641a797
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-24 22:17:12 +0100

    restyled with prettier rules

--- src/layouts/ProtectedRoute.jsx
+++ src/layouts/ProtectedRoute.jsx
@@ -1,23 +1,22 @@
-import {Outlet, useNavigate} from "react-router-dom";
-import Header from "../components/Header.jsx";
-import Footer from "../components/Footer.jsx";
-import AccountHeader from "../components/AccountHeader.jsx";
+import { Outlet, useNavigate } from "react-router-dom"
+import Footer from "../components/Footer.jsx"
+import AccountHeader from "../components/AccountHeader.jsx"
 
 export default function ProtectedRoute() {
-	const loginPage = "/login";
-	const isLoggedIn = true
+    const loginPage = "/login"
+    const isLoggedIn = true
 
-	const navigate = useNavigate();
+    const navigate = useNavigate()
 
-	if (!isLoggedIn) {
-		navigate(loginPage);
-	} else {
-		return (
-			<>
-				<AccountHeader/>
-				<Outlet/>
-				<Footer/>
-			</>
-		)
-	}
+    if (!isLoggedIn) {
+        navigate(loginPage)
+    } else {
+        return (
+            <>
+                <AccountHeader />
+                <Outlet />
+                <Footer />
+            </>
+        )
+    }
 }
