a5a64135696ca06eb7f318591da3fe10b1ec170a
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-26 12:45:40 +0100

    restyled with prettier rules

--- src/utils/shortkeys.js
+++ src/utils/shortkeys.js
@@ -1,33 +1,32 @@
 export const shortkeyFunctions = {
     playerPlayPause: () => {
-        console.log('Playing or Pausing');
+        console.log("Playing or Pausing")
     },
     playerTenForward: (time) => {
-        console.log('Seeking to', time);
+        console.log("Seeking to", time)
     },
     playerTenBack: (time) => {
-        console.log('Seeking to', time);
+        console.log("Seeking to", time)
     },
     playerReplayCurrentNote: () => {
-        console.log('Replaying current note');
+        console.log("Replaying current note")
     },
     playerJumpToNextNote: () => {
-        console.log('Jumping to next note');
+        console.log("Jumping to next note")
     },
     playerJumpToPreviousNote: () => {
-        console.log('Jumping to previous note');
+        console.log("Jumping to previous note")
     },
     playerMute: () => {
-        console.log('Muting');
+        console.log("Muting")
     },
     playerSwitchSubtitles: () => {
-        console.log('Switching subtitles');
+        console.log("Switching subtitles")
     },
     noteAddNote: () => {
-        console.log('Adding note');
+        console.log("Adding note")
     },
     noteFocusDescription: () => {
-        console.log('Focusing description');
-
-    }
+        console.log("Focusing description")
+    },
 }
