e53b100f9f1204ca4d04f171ad742e1bbbcd414c
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-17 23:14:45 +0100

    added creating spaces

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-import addVideoDocumentation from "../data/content/documentation/add-video.js";
+import addVideoDocumentation from "../data/content/documentation/add-video.js"
 
 export const documentationObject = [addVideoDocumentation]
+
+// ToDo: Replace token calls with helper vars:
+export const getAccessToken =
+    JSON.parse(sessionStorage.getItem("vud"))?.at || "defaultAccessToken"
+export const getRefreshToken =
+    JSON.parse(localStorage.getItem("vrt"))?.rt || "defaultRefreshToken"
