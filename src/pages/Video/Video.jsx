1ca47e68487bfae1b248719ccdabf536094255fb
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-31 14:07:58 +0100

    refactored video embed into its own component

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -9,6 +9,7 @@ import NoteDescription from "./NoteDescription.jsx"
 import VideoTags from "./VideoTags.jsx"
 import VideoCategory from "./VideoCategory.jsx"
 import NotesSection from "./NotesSection.jsx"
+import VideoEmbed from "./VideoEmbed.jsx"
 
 export default function Video() {
     const { videoId } = useParams()
@@ -97,16 +98,8 @@ export default function Video() {
                         <div className="flex w-2/3 flex-col gap-8">
                             {/*Video*/}
 
-                            <div className="relative h-0 w-full pb-[56.25%]">
-                                <iframe
-                                    src="https://www.youtube.com/embed/SqcY0GlETPk?si=IoUeiCrkUBvlnYXI?rel=0&modestbranding=1&showinfo=0&controls=1"
-                                    title="YouTube video player"
-                                    frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                    referrerPolicy="strict-origin-when-cross-origin"
-                                    allowFullScreen
-                                    className="absolute left-0 top-0 h-full w-full"></iframe>
-                            </div>
+                            <VideoEmbed />
+
                             <div>
                                 <NoteDescription
                                     activeVideoNote={activeVideoNote}
