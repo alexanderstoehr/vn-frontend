76879a85273c5aa18d95dbee2aed9f90dce753df
Alexander Stöhr <git@alexander-stoehr.de>
2025-01-03 19:37:48 +0100

    fixed sentry org

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { sentryVitePlugin } from "@sentry/vite-plugin"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "alexander-stohr",
-    project: "veenotes"
-  })],
+    plugins: [
+        react(),
+        sentryVitePlugin({
+            org: "alexander-stoehr",
+            project: "veenotes",
+        }),
+    ],
 
-  build: {
-    sourcemap: true
-  }
+    build: {
+        sourcemap: true,
+    },
 })
