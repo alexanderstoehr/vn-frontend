b0a0f21cf97839c22c95f3781373997c55762e9f
Alexander Stöhr <git@alexander-stoehr.de>
2025-01-03 17:27:14 +0100

    added sentry environment tags

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import * as Sentry from "@sentry/react"
 Sentry.init({
     dsn: "https://995a6e5980ba1e7fbb9cfda1e351b711@o4508088476565504.ingest.de.sentry.io/4508518026182736",
     integrations: [],
+    environment: import.meta.env.SENTRY_ENVIRONMENT,
 })
 
 createRoot(document.getElementById("root")).render(
