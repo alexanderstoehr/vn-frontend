28368b7ea3c8bfaeedd3013b4d22313b86b23e00
Alexander Stöhr <git@alexander-stoehr.de>
2025-01-03 17:18:19 +0100

    added allCategories fetching to populate options

--- src/pages/Video/VideoCategory.jsx
+++ src/pages/Video/VideoCategory.jsx
@@ -1,9 +1,53 @@
 import HelpLabel from "../../components/primitives/HelpLabel.jsx"
 import InputSelect from "../../components/primitives/InputSelect.jsx"
+import { useGetAllCategories } from "../../hooks/useGetAllCategories.jsx"
+import { useEffect, useState } from "react"
+import { useEditVideoMutation } from "../../hooks/useEditVideoMutation.jsx"
+import { useParams } from "react-router-dom"
 
-export default function VideoCategory({ videoCategories }) {
+export default function VideoCategory({ videoCategory }) {
+    const [categoryOptions, setCategoryOptions] = useState([]) // Array of category objects
+    const [selectedCategory, setSelectedCategory] = useState(videoCategory) // Object of selected category
+
+    const { videoId } = useParams()
+
+    const allCategories = useGetAllCategories()
+    const patchVideoCategory = useEditVideoMutation()
+
+    useEffect(() => {
+        console.log("All Categories: ", allCategories.data)
+        console.log("Video category: ", videoCategory)
+        if (allCategories.data) {
+            const catObj = allCategories.data.map((category) => ({
+                id: category.id,
+                label: category.category_name,
+                value: category.id,
+                key: category.id,
+            }))
+            setCategoryOptions(catObj)
+            setSelectedCategory(videoCategory)
+            console.log("selected category: ", selectedCategory)
+        }
+    }, [allCategories.data, videoCategory])
+
+    console.log("video id: ", videoId)
+    console.log("category selected: ", selectedCategory)
+    console.log("video category: ", videoCategory)
+    console.log("categoryOptions", categoryOptions)
+
+    const categoryPayload = {
+        id: videoId,
+        video_id: videoId,
+        category_id: selectedCategory.id,
+    }
+
     const onChange = (e) => {
+        // setSelectedCategory(
+        //     categoryOptions.find((option) => option.id === e.target.value)
+        // )
+        console.log("onchange", selectedCategory, "video id: ", videoId)
         console.log(e)
+        //patchVideoCategory.mutate(categoryPayload)
     }
 
     return (
@@ -12,11 +56,13 @@ export default function VideoCategory({ videoCategories }) {
                 <div className="mr-4 text-lg font-semibold">Video Category</div>
                 <HelpLabel text="Add a category to help categorize your videos" />
             </div>
-            <InputSelect
-                options={videoCategories.categories}
-                selectedValueID="3"
-                onChange={onChange}
-            />
+            {videoCategory && allCategories.data && (
+                <InputSelect
+                    options={categoryOptions}
+                    onChange={onChange}
+                    selectedValueID={selectedCategory.id}
+                />
+            )}
         </div>
     )
 }
