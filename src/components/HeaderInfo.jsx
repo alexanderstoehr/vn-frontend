3037f6c87197135f1a7a22bf2a85f6807b10e1b6
Alexander Stöhr <git@alexander-stoehr.de>
2025-01-08 16:11:15 +0100

    added version modal content and visibility states

--- src/components/HeaderInfo.jsx
+++ src/components/HeaderInfo.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
+import VersionInfoModal from "./modals/VersionInfoModal.jsx"
+
 export default function HeaderInfo() {
+    const [showVersionInfoModal, setShowVersionInfoModal] = useState(true)
+
     return (
         <div>
+            {showVersionInfoModal && (
+                <VersionInfoModal
+                    setShowVersionInfoModal={setShowVersionInfoModal}
+                />
+            )}
             <img
                 src="/assets/images/alpha.png"
                 alt="Veenotes Development Progress"
+                onClick={() => setShowVersionInfoModal(!showVersionInfoModal)}
+                className="cursor-pointer"
             />
         </div>
     )
